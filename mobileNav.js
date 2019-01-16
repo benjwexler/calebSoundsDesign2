02fded87def0f07831301465844a80c451906994
Ben Wexler <mixedywex@gmail.com>
2019-01-16 14:50:51 -0500

    different dropdowns starting to behave properly

--- mobileNav.js
+++ mobileNav.js
@@ -1,10 +1,17 @@
 let mobileDropdown = document.getElementById("mobileLinksContainer")
+let navDropdownOuterContainer = document.getElementById("navDropdownOuterContainer")
 
 toggleMobileNav = () => {
     console.log("mobile nav clicked")
     if(mobileDropdown.style.marginTop !== "0px"){
+        
+        // navDropdownOuterContainer.style.display = "block"
+        navDropdownOuterContainer.style.zIndex = "9999999";
+
     mobileDropdown.style.marginTop = "0px";
     } else {
+        // navDropdownOuterContainer.style.display = "none"
+        navDropdownOuterContainer.style.zIndex = "-9999999";
         mobileDropdown.style.marginTop = "-475px"; 
     }
 }
